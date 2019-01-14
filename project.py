e15dd0f75a183e69c3675d484ff1226f5a8f8e7c
dish34 <37023998+dish34@users.noreply.github.com>
2019-01-14 20:57:25 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,14 @@
 import time
 import webbrowser
+
 Total_hrs=int(input("Total number of hours you want to study:\n "))
 Take_break=0
 while (Take_break<Total_hrs):
                    webbrowser.open("https://youtu.be/r1Ar_QGt2Ho")
                    time.sleep(3600)
                    Take_break+=1
+
+        
+        
+        
 #press ctrl-c to exit
