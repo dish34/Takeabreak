23e1535d9d136685ce59bb891c6b7dcd0295a7ed
dish34 <37023998+dish34@users.noreply.github.com>
2019-02-01 15:13:34 +0530

    Add files via upload

---
+++ Takebreak_on_beep.py
@@ -0,0 +1,9 @@
+import time
+import winsound
+
+Total_hrs=int(input("Total number of hours you want to study:\n "))
+Take_break=0
+while (Take_break<Total_hrs):
+                   time.sleep(3600)
+                   winsound.Beep(3475,1000)
+                   Take_break+=1
